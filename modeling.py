781e06fc8651d469da91b3dc5b6a3409ddd57635
TinaSun4 <wxs172330@utdallas.edu>
2018-09-17 23:37:27 -0500

    runs skLearn linear_model

--- modeling.py
+++ modeling.py
@@ -11,15 +11,52 @@ raw2=raw1._get_numeric_data()
 raw3=pd.DataFrame(data=raw2)
 #fill missing value with median
 raw4=raw3.fillna(raw.median())
-raw4
-'''writer=pd.ExcelWriter('C:\\Users\\Tina Sun\\Desktop\\term3\\machine learning\\machine-learning\\11.xlsx')
-raw4.to_excel(writer,'Sheet1')
-writer.save()'''
+#raw4
 
 # write a function that runs skLearn linear_model iteratively
-
-
 # and at each iteration goes through stepwise process to choose the variables.
 # Use a level of significance of 0.05 for critical value.
 # Use your function to build a linear model on the dataset that will be emailed to you. 
-# Before building the model,impute all missing values to the median of that variable and cap and floor outliers at 0.1% and 99.9%.
+import statsmodels.api as sm
+X = raw4.loc[:,raw4.columns!='oiadp']
+y = raw4['oiadp']
+def stepwise(X,y,          
+             initial_list=[],
+             threshold_in=0.05,
+             threshold_out=0.05,
+             verbose=True):
+    included=list(initial_list)
+    while True:
+        changed=False
+        #forward step
+        excluded=list(set(X.columns)-set(included))
+        new_pval=pd.Series(index=excluded)
+        for new_column in excluded:
+            model=sm.OLS(y,sm.add_constant(pd.DataFrame(X[included+[new_column]]))).fit()
+            new_pval[new_column]=model.pvalues[new_column]
+        best_pval=new_pval.min()
+        if best_pval < threshold_in:
+            best_feature = new_pval.argmin()
+            included.append(best_feature)
+            changed= True
+            if verbose:
+                print('Add {:30} with p-value{:.6}'.format(best_feature,best_pval))
+       #backward step
+        model=sm.OLS(y,sm.add_constant(pd.DataFrame(X[included]))).fit()
+        #use all coefs except intercept
+        pvalues=model.pvalues.iloc[1:]
+        worst_pval=pvalues.max()
+        if worst_pval > threshold_out:
+            changed=True
+            worst_feature=pvalues.argmax()
+            included.remove(worst_feature)
+            if verbose:
+                print('drop {:30} with p=value{:.6}'.format(worst_feature,worst_pval))
+        if not changed:
+            break
+        return included
+result=stepwise(X,y)
+print('operational profits:')
+print(result)
+
+
